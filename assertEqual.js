b51f887812cac73d9ee619ec02b1e86612fe6f39
siopao2001 <sio_pao2001@yahoo.ca>
2021-07-22 16:27:56 +0000

    Template literals added

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,11 @@
 const assertEqual = function(actual, expected) {
 	if (actual === expected) {
-	    console.log("😄 Assertion Passed: " + actual + " === " + expected)
+	    console.log(`😄 Assertion Passed: ${actual}` + ` === ${expected}`)
 	} else if (actual !== expected) {
-	    console.log("🤯 Assertion Failed: " + actual + " !== " + expected)
+	    console.log(`🤯 Assertion Failed:  + ${actual}` + ` !==  ${expected}`)
 	}
     };
     
     
     assertEqual("Lighthouse Lbs", "Bootcamp");
-    assertEqual(1, 1);
+    assertEqual(1, 1)
