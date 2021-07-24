363e1344816e8a6a61f76e63c3c9676ea0d77363
siopao2001 <sio_pao2001@yahoo.ca>
2021-07-24 18:27:37 +0000

    Created function to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+	if (actual === expected) {
+	  console.log(`😄 Assertion Passed: ${actual}` + ` === ${expected}`)
+	} else if (actual !== expected) {
+	  console.log(`🤯 Assertion Failed:  + ${actual}` + ` !==  ${expected}`)
+	}
+      };
+     
+     
+      const eqArrays = function(arr1, arr2) {
+     
+     
+	if (arr1.length !== arr2.length) return false;
+     
+     
+	for (var i = 0; i < arr1.length; i++) {
+	  if (arr1[i] !== arr2[i]) return false;
+	}
+     
+     
+	return true;
+     
+      };
+     
+      console.log(eqArrays([1, 2, 4], [1, 2, 4]));
+      assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
