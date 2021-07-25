091f82ec2adaa5a44fbbf43fc28ff9e75c7e01e0
siopao2001 <sio_pao2001@yahoo.ca>
2021-07-25 00:26:12 +0000

    Function for flattening arrays

---
+++ flatten.js
@@ -0,0 +1,15 @@
+const flatten = function(arr) {
+	let newArray = [];
+	for (let i = 0; i < arr.length; i++) {
+	  if (Array.isArray(arr[i])) {
+	    for (let b = 0; b < arr[i].length; b++) {
+	      newArray.push(arr[i][b]);
+	    }
+	  } else {
+	    newArray.push(arr[i]);
+	  }
+	}
+	return newArray;
+      };
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]))
