ac604d4292571f73384e50ecd3953a6a942f3495
siopao2001 <sio_pao2001@yahoo.ca>
2021-07-27 23:01:13 +0000

    Created letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,14 @@
+const letterPositions = function (sentence) {
+	const results = {};
+	for (letters in sentence) {
+		if (sentence[letters] !== ' ') {
+			if (results[sentence[letters]]) {
+				results[sentence[letters]].push(+letters);
+			} else {
+				results[sentence[letters]] = [+letters]
+			}
+		}
+	}
+	return results;
+};
+console.log(letterPositions("lighthouse in the house"))
