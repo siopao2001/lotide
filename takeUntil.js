16ba4d681e80805e214c9168b73d29012b48c53c
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-10 22:25:34 +0000

    Added modularity

--- takeUntil.js
+++ takeUntil.js
@@ -1,17 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (var i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`😄 Assertion Passed: ${arr1}` + ` === ${arr2}`)
-  } else {
-    console.log(`🤯 Assertion Failed:  ${arr1}` + ` !==  ${arr2}`)
-  }
-}
 const takeUntil = function(array, callback) {
   let newArray = [];
   for (let item = 0; item < array.length; item++) {
@@ -22,13 +8,7 @@ const takeUntil = function(array, callback) {
   }
   return newArray
 }
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-console.log('---');
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+
 
 
 module.exports = takeUntil
