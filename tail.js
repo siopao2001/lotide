7b40bdf534248cac2d90fb81dbdb98acc657df42
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-10 12:58:13 +0000

    Refactored for modularity

--- tail.js
+++ tail.js
@@ -1,14 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😄 Assertion Passed: ${actual}` + ` === ${expected}`)
-  } else if (actual !== expected) {
-    console.log(`🤯 Assertion Failed:  + ${actual}` + ` !==  ${expected}`)
-  }
-};
+const assertEqual = require('./assertEqual')
+
+
 const tail = function(array) {
   return array.slice(1)
 }
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+
+
+module.exports = tail
