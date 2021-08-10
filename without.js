2e785dd29a6e80fc6ea2757832400ff56510e7d7
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-10 22:27:22 +0000

    Added modularity

--- without.js
+++ without.js
@@ -1,17 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (var i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`😄 Assertion Passed: ${arr1}` + ` === ${arr2}`)
-  } else {
-    console.log(`🤯 Assertion Failed:  ${arr1}` + ` !==  ${arr2}`)
-  }
-}
 const without = function(source, itemsToremove) {
   const newArray = [];
   for (var i = 0; i < source.length; i++) {
@@ -21,10 +7,8 @@ const without = function(source, itemsToremove) {
   }
   return newArray;
 };
-console.log(without([1, 2, 3], [1])) // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+
 
 
 module.exports = without
 
-test
