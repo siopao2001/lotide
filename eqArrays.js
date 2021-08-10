54bbed33077fc9cc5d6c1d73ec98f9f84004c799
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-10 13:12:43 +0000

    Refactored for modularity

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😄 Assertion Passed: ${actual}` + ` === ${expected}`)
-  } else if (actual !== expected) {
-    console.log(`🤯 Assertion Failed:  ${actual}` + ` !==  ${expected}`)
-  }
-};
+const assertEqual = require('./assertEqual');
+
+
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   for (var i = 0; i < arr1.length; i++) {
@@ -12,6 +8,7 @@ const eqArrays = function(arr1, arr2) {
   }
   return true;
 };
-console.log(eqArrays([1, 2, 4], [1, 2, 4]));
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, "blue"], [1, 2, 3]), true)
+
+
+
+module.exports = eqArrays
