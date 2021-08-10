bd9d508a362a604ae6d9a2fd716395499401f791
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-10 20:43:53 +0000

    Added modularity

--- takeUntil.js
+++ takeUntil.js
@@ -30,4 +30,5 @@ const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Re
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
+
 module.exports = takeUntil
