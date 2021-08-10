a7b68e819f9c257e817f8a64e69be011d3f2ac86
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-10 22:19:43 +0000

    Added modularity

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,23 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😄 Assertion Passed: ${actual}` + ` === ${expected}`)
-  } else if (actual !== expected) {
-    console.log(`🤯 Assertion Failed:  + ${actual}` + ` !==  ${expected}`)
-  }
-};
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire",
-  Horror: "It"
-};
 const findKeyByValue = function(object, value) {
   for (key in object) {
     if (object[key] === value) return key;
   }
 };
-console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
 
 module.exports = findKeyByValue
