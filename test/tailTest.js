fddb51f33b2ce43fdcd3e31c68862eb41f6586d3
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-10 14:51:19 +0000

    Incorporated Mocha and Chai Testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail')
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+const result = ["Hello", "Lighthouse", "Labs"];
+
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(result), ['Lighthouse', 'Labs']);
+  });
+  it("Original array should not be modified", () => {
+    assert.deepEqual(result, ['Hello', 'Lighthouse', 'Labs']);
+  });
+});
+
