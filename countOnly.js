8039395781b6ff8ee90c6215395e90f9204d2008
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-10 22:16:59 +0000

    Added modularity

--- countOnly.js
+++ countOnly.js
@@ -1,27 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😄 Assertion Passed: ${actual}` + ` === ${expected}`)
-  } else if (actual !== expected) {
-    console.log(`🤯 Assertion Failed:  + ${actual}` + ` !==  ${expected}`)
-  }
-};
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-const neededObjects = {
-  "Jason": true,
-  "Karima": true,
-  "Fang": true,
-  "Agouhanna": false
-}
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
@@ -35,6 +11,5 @@ const countOnly = function(allItems, itemsToCount) {
   }
   return results
 }
-console.log(countOnly(firstNames, neededObjects))
 
 module.exports = countOnly
