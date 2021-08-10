6878454c9fb6899d734df1817c5dc29b49bc3c0f
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-10 22:18:13 +0000

    Added modularity

--- findKey.js
+++ findKey.js
@@ -1,34 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😄 Assertion Passed: ${actual}` + ` === ${expected}`)
-  } else if (actual !== expected) {
-    console.log(`🤯 Assertion Failed:  + ${actual}` + ` !==  ${expected}`)
-  }
-};
 const findKey = function(object, callback) {
   for (key in object) {
     if (callback(object[key]) == true) return key;
   }
 };
-console.log(findKey({
-    "Blue Hill": {
-      stars: 1
-    },
-    "Akaleri": {
-      stars: 3
-    },
-    "noma": {
-      stars: 2
-    },
-    "elBulli": {
-      stars: 3
-    },
-    "Ora": {
-      stars: 2
-    },
-    "Akelarre": {
-      stars: 3
-    }
-  }, x => x.stars === 2)) // => "noma"
+
+
 
 module.exports = findKey
