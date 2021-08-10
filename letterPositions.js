57e85b4e97a89a75d1a75dd2fc6e6b275469e159
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-10 22:22:58 +0000

    Added modularity

--- letterPositions.js
+++ letterPositions.js
@@ -1,17 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (var i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`😄 Assertion Passed: ${arr1}` + ` === ${arr2}`)
-  } else {
-    console.log(`🤯 Assertion Failed:  ${arr1}` + ` !==  ${arr2}`)
-  }
-}
 const letterPositions = function(sentence) {
   const results = {};
   for (letters in sentence) {
@@ -25,7 +11,8 @@ const letterPositions = function(sentence) {
   }
   return results;
 };
-console.log(letterPositions("lighthouse in the house"))
+
+
 
 
 module.exports = letterPositions
